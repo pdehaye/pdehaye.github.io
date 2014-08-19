52f627e258635d2534eeb33cb9b5bf7a295827a6
Paul-Olivier Dehaye <paul-olivier.dehaye@math.uzh.ch>
2014-08-19 12:21:35 +0200

    Changed disqus comment

--- src/conf.py
+++ src/conf.py
@@ -468,7 +468,7 @@ COMMENT_SYSTEM = "disqus"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "pdehaye-fivebyfive"
+COMMENT_SYSTEM_ID = "pdehaye-blog"
 
 # Enable annotations using annotateit.org?
 # If set to False, you can still enable them for individual posts and pages
