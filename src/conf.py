481820436be3b70c44c86a51e93e19b3e0aa2328
Paul-Olivier Dehaye <paul-olivier.dehaye@math.uzh.ch>
2014-08-19 12:54:28 +0200

    Updating blog

--- src/conf.py
+++ src/conf.py
@@ -281,7 +281,7 @@ REDIRECTIONS = []
 # And then do a backup, or run `nikola ping` from the `ping`
 # plugin (`nikola install_plugin ping`).
 # To do manual deployment, set it to []
-# DEPLOY_COMMANDS = []
+DEPLOY_COMMANDS = ["git add --all","git commit -m 'Updating blog'","git push"]
 
 # For user.github.io/organization.github.io pages, the DEPLOY branch
 # MUST be 'master', and 'gh-pages' for other repositories.
