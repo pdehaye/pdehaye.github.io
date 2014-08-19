45eb70255ebdad778ad81f1f7949f28db358e022
Paul-Olivier Dehaye <paul-olivier.dehaye@math.uzh.ch>
2014-08-19 23:58:02 +0200

    Updating blog

--- src/conf.py
+++ src/conf.py
@@ -663,6 +663,7 @@ SEARCH_FORM = """
 # before </head>
 # (translatable)
 EXTRA_HEAD_DATA = """
+
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
